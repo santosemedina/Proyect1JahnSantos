8d12a73042e2fe460fc447747813335129976136
Santos <Santos@10.31.3.131>
2016-10-11 14:45:44 -0400

    Changed winning and checking algorithm

--- MineSweeperJahnSantos/src/Main.java
+++ MineSweeperJahnSantos/src/Main.java
@@ -4,11 +4,11 @@
 // October/14/2016
 // 
 
-import java.awt.Color;
+
 import java.util.Random;
 
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+
 
 public class Main {
 	public static void main(String[] args) {
